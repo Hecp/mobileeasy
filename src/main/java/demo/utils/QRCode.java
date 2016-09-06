e22980a3b625e97d44d007f7bf2eebc7c145cf23
hechengping <hechengping@11wlw.com>
2016-09-06 18:00:56 +0800

    增加二维码图片生成工具类

---
+++ src/main/java/demo/utils/QRCode.java
@@ -0,0 +1,160 @@
+package demo.utils;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * 二维码工具类
+ * Created by hecp on 2016/9/6.
+ */
+public class QRCode {
+    private static final Logger LOGGER = LoggerFactory.getLogger(QRCode.class);
+    // LOGO宽度
+    private static final int WIDTH = 60;
+    // LOGO高度
+    private static final int HEIGHT = 60;
+    // 二维码尺寸
+    private static final int QRCODE_SIZE = 300;
+    // 字符编码
+    private static final String CHARSET = "UTF-8";
+    // 图片格式
+    private static final String FORMAT_NAME = "PNG";
+
+    /**
+     * 生成二维码图片
+     */
+    public static void generateQRCodeImg(JSONObject proper, int width, int height, String filePath, String fileName, String format) {
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        try {
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(proper.toString(), BarcodeFormat.QR_CODE,
+                    width, height, hints);
+            Path path = FileSystems.getDefault().getPath(filePath, fileName + "." + format);
+            /* MatrixToImageWriter.writeToStream(); ——输出二维码二进制流，可以直接用response.getOutputStream
+            输出到页面显示*/
+            MatrixToImageWriter.writeToPath(bitMatrix, format, path);
+            LOGGER.debug("generate QR code success!");
+        } catch (WriterException | IOException e) {
+            LOGGER.error("generate QR code error!", e);
+        }
+    }
+
+    private static BufferedImage createImage(String content, String imgPath,
+                                             boolean needCompress) throws Exception {
+        Hashtable<EncodeHintType, Object> hints = new Hashtable<>();
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hints.put(EncodeHintType.CHARACTER_SET, CHARSET);
+        hints.put(EncodeHintType.MARGIN, 1);
+        BitMatrix bitMatrix = new MultiFormatWriter().encode(content,
+                BarcodeFormat.QR_CODE, QRCODE_SIZE, QRCODE_SIZE, hints);
+        int width = bitMatrix.getWidth();
+        int height = bitMatrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000
+                        : 0xFFFFFFFF);
+            }
+        }
+        if (imgPath == null || "".equals(imgPath)) {
+            return image;
+        }
+        // 插入图片
+        insertImage(image, imgPath, needCompress);
+        return image;
+    }
+
+    /**
+     * 插入LOGO
+     *
+     * @param source       二维码图片
+     * @param imgPath      LOGO图片地址
+     * @param needCompress 是否压缩
+     * @throws Exception
+     */
+    private static void insertImage(BufferedImage source, String imgPath,
+                                    boolean needCompress) throws Exception {
+        File file = new File(imgPath);
+        if (!file.exists()) {
+            LOGGER.debug(imgPath + "该文件不存在！");
+            return;
+        }
+        Image src = ImageIO.read(new File(imgPath));
+        int width = src.getWidth(null);
+        int height = src.getHeight(null);
+        if (needCompress) { // 压缩LOGO
+            if (width > WIDTH) {
+                width = WIDTH;
+            }
+            if (height > HEIGHT) {
+                height = HEIGHT;
+            }
+            Image image = src.getScaledInstance(width, height,
+                    Image.SCALE_SMOOTH);
+            BufferedImage tag = new BufferedImage(width, height,
+                    BufferedImage.TYPE_INT_RGB);
+            Graphics g = tag.getGraphics();
+            g.drawImage(image, 0, 0, null); // 绘制缩小后的图
+            g.dispose();
+            src = image;
+        }
+        // 插入LOGO
+        Graphics2D graph = source.createGraphics();
+        int x = (QRCODE_SIZE - width) / 2;
+        int y = (QRCODE_SIZE - height) / 2;
+        graph.drawImage(src, x, y, width, height, null);
+        Shape shape = new RoundRectangle2D.Float(x, y, width, width, 6, 6);
+        graph.setStroke(new BasicStroke(3f));
+        graph.draw(shape);
+        graph.dispose();
+    }
+
+    /**
+     * 生成二维码(内嵌LOGO)
+     *
+     * @param content      内容
+     * @param imgPath      LOGO地址
+     * @param destPath     存放目录
+     * @param needCompress 是否压缩LOGO
+     * @throws Exception
+     */
+    public static void encode(String content, String imgPath, String destPath,
+                              boolean needCompress) throws Exception {
+        BufferedImage image = createImage(content, imgPath,
+                needCompress);
+        mkdirs(destPath);
+        String file = new Random().nextInt(99999999) + "." + FORMAT_NAME;
+        ImageIO.write(image, FORMAT_NAME, new File(destPath + "/" + file));
+        LOGGER.debug("QR code generate success! " + file);
+    }
+
+    private static void mkdirs(String destPath) {
+        File file = new File(destPath);
+        //当文件夹不存在时，mkdirs会自动创建多层目录，区别于mkdir．(mkdir如果父目录不存在则会抛出异常)
+        if (!file.exists() && !file.isDirectory()) {
+            file.mkdirs();
+        }
+    }
+}
